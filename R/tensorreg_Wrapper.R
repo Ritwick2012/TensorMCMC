66d2dd97a67fda1b82517066ef4e32bddfa7abe3
Ritwick Mondal <ritwick12@tamu.edu>
2025-11-19 00:55:38 -0600

    Add predict.tensor.reg S3 method for tensor regression predictions

--- R/tensorreg_Wrapper.R
+++ R/tensorreg_Wrapper.R
@@ -82,3 +82,53 @@ tensor.reg <- function(z.train, x.train, y.train, nsweep=50, rank=2, scale=TRUE,
   class(out) <- "tensor.reg"
   return(out)
 }
+
+#' Prediction from tensor regression (S3 method)
+#'
+#' @param object tensor.reg object
+#' @param x.new new tensor predictors (n x p x d)
+#' @param z.new new scalar covariates (n x pgamma)
+#' @param scale Logical; whether to scale predictors
+#' @param ... additional arguments
+#' @return Predicted response vector
+#' @export
+#' @method predict tensor.reg
+predict.tensor.reg <- function(object, x.new, z.new, scale=TRUE, ...) {
+
+  fit <- object
+
+  n <- dim(x.new)[1]
+  p <- dim(x.new)[2]
+  d <- dim(x.new)[3]
+
+  Xt <- x.new
+  if(scale){
+    for(j in 1:p){
+      for(k in 1:d){
+        colx <- Xt[,j,k]
+        rng <- diff(range(colx))
+        if(rng > 0) Xt[,j,k] <- (colx - mean(colx)) / rng
+      }
+    }
+  }
+
+  vecXt <- as.numeric(aperm(Xt, c(1,2,3)))
+  beta.mean <- apply(fit$beta.store, c(2,3,4), mean)
+
+
+  if(fit$rank == 1) {
+    beta_list <- list(matrix(beta.mean[1,,], nrow=p, ncol=d))
+  } else {
+    beta_list <- lapply(1:fit$rank, function(r) matrix(beta.mean[r,,], nrow=p, ncol=d))
+  }
+
+  gam.mean <- colMeans(fit$gam.store)
+
+  # C++ prediction
+  pred <- predict_tensor_cpp(vecXt, beta_list, gam.mean, n, p, d, fit$rank)
+
+  # Add scalar covariates contribution
+  pred <- pred + rowSums(z.new * matrix(gam.mean, nrow=n, ncol=length(gam.mean), byrow=TRUE))
+
+  return(pred)
+}
